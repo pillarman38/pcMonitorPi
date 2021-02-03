fc8e964626613fa298259d627cb9f6a0bce855b2
pillarman38 <connorwoodford@yahoo.com>
2021-02-03 00:30:03 -0600

    changes in design

--- client.js
+++ client.js
@@ -1,25 +1,35 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const url = require("url");
 const path = require("path");
+var express = require('express')
+const application = express();
 var port = 4002
-
+var os = require('os')
+var fs = require('fs')
+var http = require('http').createServer(application);
 const { exec, spawn } = require('child_process');
-// const wmic = require('wmic');
 var cors = require('cors')
 
 let mainWindow
 
 application.use(cors())
 
+
+
+http.listen(port|| 3000, function() {
+  var host = http.address().address
+  var port = http.address().port
+  console.log('App listening at http://%s:%s', host, port)
+});
+
+
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       nodeIntegration: false
     }
   })
-
+  mainWindow.setKiosk(true);
   mainWindow.loadURL("http://192.168.1.86:4002");
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
